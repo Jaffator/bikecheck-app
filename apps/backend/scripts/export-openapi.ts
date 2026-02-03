26d096d1a0baf70dab571f33faba7951c4fc4909
Jaffa <jardalufi@gmail.com>
2026-02-03 22:53:45 +0100

    feat: improve OpenAPI schema export error handling and validation

--- apps/backend/scripts/export-openapi.ts
+++ apps/backend/scripts/export-openapi.ts
@@ -5,15 +5,18 @@ import chalk from 'chalk';
 async function getApiSchema(): Promise<void> {
   const res = await fetch('http://localhost:3000/api-json');
   const json = await res.json();
+  const schema = JSON.stringify(json, null, 2);
+  if (schema.length < 100) {
+    throw new Error('Fetched schema is probably not contain full api schema');
+  }
   const outputPath = path.join(__dirname, '..', 'bikecheckapi_schema.json');
-  await fs.writeFile(outputPath, JSON.stringify(json, null, 2));
-
+  await fs.writeFile(outputPath, schema);
   console.log(
     chalk.yellow.bgGreen.bold(`--- OpenAPI schema successfully exported ---`),
   );
 }
 
 getApiSchema().catch((err) => {
-  console.error('Error:', err);
+  console.error(chalk.yellow.bgRed.bold('Error:', err.message));
   process.exit(1);
 });
