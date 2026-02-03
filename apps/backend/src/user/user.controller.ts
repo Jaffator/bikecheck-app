cf5cfc0224b77fd28451c20c1ba466519d610474
Jaffa <jardalufi@gmail.com>
2026-02-03 22:55:13 +0100

    a

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -38,6 +38,13 @@ export class UserController {
     return { message: 'shit works' };
   }
 
+  // GET user/:id
+  @ApiOperation({ summary: 'TEST' })
+  @ApiResponse({ status: 200, type: UserResponseDto })
+  @Get('aaa')
+  aaa() {
+    return { message: 'aaa works' };
+  }
   // POST new user
   @ApiOperation({ summary: 'Create a new user' })
   @ApiResponse({ status: 201, type: UserResponseDto })
